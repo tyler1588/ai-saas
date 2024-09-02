4b8e78256684a61d559febbfec85c72d7eb98afb
Tyler Jackson <87100297+tyler1588@users.noreply.github.com>
2024-09-02 09:36:19 -0400

    add dashboard

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
       <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-900">
         <Sidebar />
       </div>
-      <main className="md:pl-72">
+      <main className="pb-12 md:pl-72">
         <Navbar />
         {children}
       </main>
